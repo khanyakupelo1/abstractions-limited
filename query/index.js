342a1990742e59b6bfe40953a47c59cd476dbb66
Khanya <sibulelekupelo@gmail.com>
2022-10-10 14:43:28 +0200

    Event async in action

--- query/index.js
+++ query/index.js
@@ -1,66 +1,64 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
 
-const handleEvents = (type, data) => {
-    if (type === 'PostCreated') {
+const handleEvent = (type, data) => {
+    if (type === "PostCreated") {
         const { id, title } = data;
 
         posts[id] = { id, title, comments: [] };
-
-        posts;
     }
 
-    if (type === 'CommentCreated') {
+    if (type === "CommentCreated") {
         const { id, content, postId, status } = data;
+
         const post = posts[postId];
         post.comments.push({ id, content, status });
     }
 
-    if (type === 'CommentUpdated') {
+    if (type === "CommentUpdated") {
         const { id, content, postId, status } = data;
 
-
         const post = posts[postId];
-
-        const comment = post.comments.find(comment => {
+        const comment = post.comments.find((comment) => {
             return comment.id === id;
         });
+
         comment.status = status;
         comment.content = content;
     }
-
 };
 
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
     const { type, data } = req.body;
 
-    const handleEvent(type, data);
+    handleEvent(type, data);
 
-    res.send({ status: "OK" });
-
+    res.send({});
 });
 
-app.listen(4002, (req, res) => {
-    console.log('listening on http://localhost:4002');
+app.listen(4002, async () => {
+    console.log("Listening on 4002");
+    try {
+        const res = await axios.get("http://localhost:4005/events");
 
-    await axios.get('http://localhost:4005/events');
-    for (const event of res.data) {
-        console.log('processing event: ', event.type);
-        handleEvents(event.type, event.data);
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
     }
-
-
 });
-
-
