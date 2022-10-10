5062591084e5c1ae685f84a6b589fb031c1a04b2
Khanya <sibulelekupelo@gmail.com>
2022-10-10 10:21:48 +0200

    Add comment moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,9 +5,11 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-axios.post('/events', (req, res) => {
 
-}).catch((err) => { console.log(err); });
+// This is a listener
+app.post('/events', (req, res) => {
+    res.send({});
+});
 
 app.listen(4003, () => {
     console.log('listening on http://localhost:4003');
