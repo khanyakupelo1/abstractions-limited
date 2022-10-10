e56340838a517b116a23960cf8f9a82f682ae849
Khanya <sibulelekupelo@gmail.com>
2022-10-10 12:26:01 +0200

    Handling moderation

--- moderation/index.js
+++ moderation/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +9,21 @@ app.use(bodyParser.json());
 
 // This is a listener
 app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'accepted';
+
+        axios.post('localhost://localhost:4002/events', {
+            type: 'commentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
     res.send({});
 });
 
