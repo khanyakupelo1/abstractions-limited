e96baa2c6a4661ed9b44cd5266b8d1c52a95969a
Khanya <sibulelekupelo@gmail.com>
2022-10-24 22:14:02 +0200

    Connects event bus and fix query deployment file

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,16 +13,16 @@ app.post("/events", (req, res) => {
     axios.post("http://posts-clusterip-service:4000/events", event).catch((err) => {
         console.log(err.message);
     });
-    // axios.post("http://comments-clusterip-service:4001/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
-    // axios.post("http://localhost:4002/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    axios.post("http://comments-clusterip-service:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post("http://moderation-service:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
 
-    // axios.post("http://localhost:4003/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    axios.post("http://localhost:4003/events", event).catch((err) => {
+        console.log(err.message);
+    });
     res.send({ status: "OK" });
 });
 
